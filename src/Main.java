f945fd2066a54d1b44624d16f840be3918aa91cf
oj <owain@hush.com>
2015-03-17 18:46:20 +0700

    Updated expected test results

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,28 @@ public class Main {
         /*
         DailyTransactions mondaySales = new DailyTransactions("17/01/2014",
                 new Transaction[] {
-                   new Sale("Red Shoes", 3, 29.99, true),
-                   new Sale("Leather Jacket", 1, 185.0, false),
-                   new Sale("Umbrella", 2, 34.99, false),
-                   new Sale("Safari Shorts", 1, 45.0, true),
-                   new Return("Gloves", 2, 18.5, "Poor Fit"),
-                   new Sale("Blue Socks", 4, 5.99, true),
-                   new Sale("Baseball Cap", 1, 19.99, false),
-                   new Return("Polo Shirt", 3, 24.0, "Damaged Collars"),
-                   new Sale("T-Shirt", 4, 29.49, true),
-                   new Sale("Cargo Pants", 2, 60.0, false),
-                   new Return("Tank Top", 1, 30.0, "Wrong Color"),
-                   new Sale("Bow Tie", 5, 8.25, true)
+                        new Sale("Red Shoes", 3, 29.99, true),
+                        new Sale("Leather Jacket", 1, 185.0, false),
+                        new Sale("Umbrella", 2, 34.99, false),
+                        new Sale("Safari Shorts", 1, 45.0, true),
+                        new Return("Gloves", 2, 18.5, "Poor Fit"),
+                        new Sale("Blue Socks", 4, 5.99, true),
+                        new Sale("Baseball Cap", 1, 19.99, false),
+                        new Return("Polo Shirt", 3, 24.0, "Damaged Collars"),
+                        new Sale("T-Shirt", 4, 29.49, true),
+                        new Sale("Cargo Pants", 2, 60.0, false),
+                        new Return("Tank Top", 1, 30.0, "Wrong Color"),
+                        new Sale("Bow Tie", 5, 8.25, true)
                 });
 
         System.out.println("Sales for " + mondaySales.getDate());
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Transaction cashSale = mondaySales.largestCashSale();
         if (cashSale != null) {
-            System.out.println("Largest Cash Sale: $" + cashSale.getTotal() + " for " + cashSale.getDescription() + " (" + cashSale.getNumItems() + " pieces)"); //40.5
+            System.out.println("Largest Cash Sale: $" + cashSale.getTotal() + " for " + cashSale.getDescription() + " (" + cashSale.getNumItems() + " pieces)"); //117.96
         }
-        System.out.println("Transaction Average:  " + mondaySales.findTransactionAverage()); //27.903833333335 ish
+        System.out.println("Transaction Average:  " + mondaySales.findTransactionAverage()); //48.35566666666666
         System.out.println("Reasons for Returns:  " + mondaySales.getReturnReasons());       //[Poor Fit, Damaged Collars, Wrong Color]
         */
-
     }
 }
